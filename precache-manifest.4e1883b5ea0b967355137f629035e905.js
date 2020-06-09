4e4c13edc0cedaafc7984d3fe7711f162336d871
natalia-lypovyk <cardinnata@gmail.com>
2020-06-09 17:55:35 +0300

    Updates

--- precache-manifest.4e1883b5ea0b967355137f629035e905.js
+++ precache-manifest.4e1883b5ea0b967355137f629035e905.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "79a0b233cfcc35f1808f51c6a3c7d2eb",
+    "revision": "76bb6a94017b93a45618dc4f5853fffb",
     "url": "./index.html"
   },
   {
-    "revision": "f71ff74ce631bb26efd5",
-    "url": "./static/js/2.1f109fe1.chunk.js"
+    "revision": "7644c9bae6f4cc40b4e9",
+    "url": "./static/js/2.1328fcc0.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "./static/js/2.1f109fe1.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.1328fcc0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4543ea317f5e838956a6",
-    "url": "./static/js/main.91ead301.chunk.js"
+    "revision": "6c65662f95868c34c338",
+    "url": "./static/js/main.39c86600.chunk.js"
   },
   {
     "revision": "b2fef534a0a7d9b28162",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.28419a5c14d3b42bb4cfa0a991433ce0.js"
+  "./precache-manifest.4e1883b5ea0b967355137f629035e905.js"
 );
 
 self.addEventListener('message', (event) => {
